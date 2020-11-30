75c9cb8e2337fdbe3e55773e0afe83c8b3150331
Travis White <traviewhite@gmail.com>
2020-11-30 15:06:30 -0600

    netlify fix

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const withTM = require('next-transpile-modules');
 module.exports = withTM({
   transpileModules: ['gsap']
 });
+
 module.exports = {
+  target: "serverless",
   devIndicators: {
     autoPrerender: false,
   },

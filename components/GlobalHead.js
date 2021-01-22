ad7d0f499d7f199b28a6cdd3340d89c9cde3b231
Travis White <traviewhite@gmail.com>
2021-01-22 11:22:22 -0600

    head update

--- components/GlobalHead.js
+++ components/GlobalHead.js
@@ -6,21 +6,50 @@ const GlobalHead = () => (
     <link hrefLang='en-US' lang='en-US' />
 
     <meta
-      name='Description'
-      content='Hi, I’m Travis!
-      I’m a front-end software engineer & designer!'
+      name='description'
+      content='A personal site for Travis White that hosts everything from current work to thoughts and ideas. Travis White is a front-end software engineer and graphic designer.'
     />
+
     <meta
       name='keywords'
       content='Travis White, traviswhite, traviewhite, travis, travie, white, web development, front-end, react, developer, graphic design, illustration, printmaking, printmaker, photography, art, design, freelancer'
     />
     <meta name='author' content='Travis White' />
-    <meta property='og:type' content="Travis White's Portfolio" />
-    <meta property='og:title' content='Travis White' />
+
+    {/* google */}
+    <meta itemprop='name' content='Travis White' />
+    <meta
+      itemprop='description'
+      content='A personal site for Travis White that hosts everything from current work to thoughts and ideas. Travis White is a front-end software engineer and graphic designer.'
+    />
+    <meta
+      itemprop='image'
+      content='http://res.cloudinary.com/twhite/image/upload/v1611333961/traviewhite_web_2021_uro61u.jpg'
+    />
+
+    {/* facebook */}
     <meta property='og:url' content='https://traviewhite.com' />
+    <meta property='og:type' content='website' />
+    <meta property='og:title' content='Travis White' />
+    <meta
+      property='og:description'
+      content='A personal site for Travis White that hosts everything from current work to thoughts and ideas. Travis White is a front-end software engineer and graphic designer.'
+    />
     <meta
       property='og:image'
-      content='https://res.cloudinary.com/twhite/image/upload/v1611333961/traviewhite_web_2021_uro61u.jpg'
+      content='http://res.cloudinary.com/twhite/image/upload/v1611333961/traviewhite_web_2021_uro61u.jpg'
+    />
+
+    {/* twitter */}
+    <meta name='twitter:card' content='summary_large_image' />
+    <meta name='twitter:title' content='Travis White' />
+    <meta
+      name='twitter:description'
+      content='A personal site for Travis White that hosts everything from current work to thoughts and ideas. Travis White is a front-end software engineer and graphic designer.'
+    />
+    <meta
+      name='twitter:image'
+      content='http://res.cloudinary.com/twhite/image/upload/v1611333961/traviewhite_web_2021_uro61u.jpg'
     />
 
     <link rel='preconnect' href='https://fonts.gstatic.com' />
@@ -31,8 +60,8 @@ const GlobalHead = () => (
     <link href='https://fonts.googleapis.com/css2?family=Anton&display=swap' rel='stylesheet' />
 
     <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    <link rel='manifest' href='../favicon/site.webmanifest' />
 
+    <link rel='manifest' href='../favicon/site.webmanifest' />
     <link rel='apple-touch-icon' sizes='180x180' href='../favicon/apple-touch-icon.png' />
     <link rel='icon' type='image/png' sizes='32x32' href='../favicon/favicon-32x32.png' />
     <link rel='icon' type='image/png' sizes='16x16' href='../favicon/favicon-16x16.png' />
